17ba464313c69a4c0de56ee42815ac24f5375819
vwxyraz <74700868+vwxyraz@users.noreply.github.com>
2021-10-06 02:02:11 +0700

    update speed

--- main.js
+++ main.js
@@ -719,7 +719,7 @@ function main() {
     // var change = [0, 0];
 
     var dy = 0;
-    var speed = 0.109;
+    var speed = 0.0109;
     function render() {
         if(dy >= 0.55 || dy <= -0.55)
             speed = -speed;
